84442c2f8378686e4ca2c22124e019d9e70b82b5
Roger Andrews <theartintheraw@gmail.com>
2024-01-04 09:36:27 -0500

    Update Scene.jsx

--- src/app/components/canvas/Scene.jsx
+++ src/app/components/canvas/Scene.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { Center, Environment } from '@react-three/drei'
+import { Center } from '@react-three/drei'
 import { r3f } from '%/helpers/global'
 import { Shirt } from './Examples'
 import ImageBorder from './ImageBorder'
@@ -13,8 +13,6 @@ export default function Scene({ ...props }) {
   return (
     <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }} gl={{ preserveDrawingBuffer: true }} {...props}>
       <ambientLight intensity={0.5} />
-      <Environment preset='city' />
-
       <CameraRig>
         <Backdrop />
         <ImageBorder />
