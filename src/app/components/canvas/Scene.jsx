22b782319f07b8b473087a02a7fd48f6f83b358d
Roger Andrews <theartintheraw@gmail.com>
2024-01-05 19:10:20 -0500

    25fov

--- src/app/components/canvas/Scene.jsx
+++ src/app/components/canvas/Scene.jsx
@@ -11,7 +11,7 @@ import * as THREE from 'three'
 export default function Scene({ ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas shadows camera={{ position: [0, 0, 0], fov: 15 }} gl={{ preserveDrawingBuffer: true }} {...props}>
+    <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }} gl={{ preserveDrawingBuffer: true }} {...props}>
       <ambientLight intensity={0.5} />
       <Environment files="/img/kloofendal_43d_clear_puresky_1k.hdr" background blur={0.5} />
       <CameraRig>
