2ddb286ee284b7fe08ee8458e4bf6216e46a507d
Roger Andrews <theartintheraw@gmail.com>
2024-01-05 20:12:39 -0500

    bg

--- src/app/components/canvas/View.jsx
+++ src/app/components/canvas/View.jsx
@@ -19,9 +19,9 @@ export function Backdrop() {
         temporal
         frames={60}
         alphaTest={0.85}
-        scale={15}
+        scale={12}
         rotation={[Math.PI / 2, 0, 0]}
-        position={[0, 0, -0.14]}
+        position={[0, 0, -0.24]}
       >
         <RandomizedLight amount={4} radius={9} intensity={0.55} ambient={0.25} position={[5, 5, -10]} />
         <RandomizedLight amount={4} radius={5} intensity={0.25} ambient={0.55} position={[-5, 5, -9]} />
