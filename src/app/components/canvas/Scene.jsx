af51eb5a34c652d35854eee589aef7173032e08d
Roger Andrews <theartintheraw@gmail.com>
2024-01-05 19:40:18 -0500

    light int down

--- src/app/components/canvas/Scene.jsx
+++ src/app/components/canvas/Scene.jsx
@@ -12,7 +12,7 @@ export default function Scene({ ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
     <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }} gl={{ preserveDrawingBuffer: true }} {...props}>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.1} />
       <Environment files="/img/kloofendal_43d_clear_puresky_1k.hdr" background blur={0.5} />
       <CameraRig>
         <Backdrop />
