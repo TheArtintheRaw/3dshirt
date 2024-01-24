211a298da937e530d328f1c85278e621557a1129
Roger Andrews <theartintheraw@gmail.com>
2024-01-24 16:08:10 -0500

    Update View.jsx
    revert view back to working

--- src/app/components/canvas/View.jsx
+++ src/app/components/canvas/View.jsx
@@ -1,13 +1,11 @@
 'use client'
 
-import { Suspense, useRef, useState } from 'react'
+import { Suspense, useRef } from 'react'
 import { AccumulativeShadows, RandomizedLight, View as ViewImpl } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import { useSnapshot } from 'valtio'
 import state from '%/store'
 import { easing } from 'maath'
-import { CustomButton } from '..'
-import { fadeAnimation } from '%/config/motion'
 
 export function Backdrop() {
   const shadows = useRef()
@@ -21,9 +19,9 @@ export function Backdrop() {
         temporal
         frames={60}
         alphaTest={0.85}
-        scale={12}
+        scale={10}
         rotation={[Math.PI / 2, 0, 0]}
-        position={[0, 0, -0.24]}
+        position={[0, 0, -0.14]}
       >
         <RandomizedLight amount={4} radius={9} intensity={0.55} ambient={0.25} position={[5, 5, -10]} />
         <RandomizedLight amount={4} radius={5} intensity={0.25} ambient={0.55} position={[-5, 5, -9]} />
@@ -33,64 +31,41 @@ export function Backdrop() {
 }
 
 export function CameraRig({ children }) {
-  const group = useRef();
-  const snap = useSnapshot(state);
-  const [isDragEnabled, setDragEnabled] = useState(true);
+  const group = useRef()
+  const snap = useSnapshot(state)
 
   useFrame((state, delta) => {
-    const isBreakpoint = window.innerWidth <= 1260;
-    const isMobile = window.innerWidth <= 600;
+    const isBreakpoint = window.innerWidth <= 1260
+    const isMobile = window.innerWidth <= 600
 
     // Set the initial position of the model
-    let targetPosition = [0, 0, 2];
-
+    let targetPosition = [0, 0, 2]
     if (snap.intro) {
       if (isBreakpoint) {
-        targetPosition = [0, 0, 2];
+        targetPosition = [0, 0, 2]
       }
 
       if (isMobile) {
-        targetPosition = [0, 0, 4];
+        targetPosition = [0, 0, 4]
       }
     } else if (isMobile) {
-      targetPosition = [0, 0, 2.5];
+      targetPosition = [0, 0, 2.5]
     } else {
-      targetPosition = [0, 0, 2];
+      targetPosition = [0, 0, 2]
     }
 
     // Set model camera position
-    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
 
+    document.addEventListener('pointermove', controlShirt)
     // Update rotation of the group only when left mouse button is pressed down
+
     function controlShirt(e) {
-      if (isDragEnabled && e.buttons === 1 && group.current) {
-        easing.dampE(group.current.rotation, [0, state.pointer.x, 0], 0.25, delta);
-      }
-      else if (!isDragEnabled && e.buttons === 1 && group.current) {
-        return
+      if (e.buttons === 1 && group.current) {
+        easing.dampE(group.current.rotation, [0, state.pointer.x, 0], 0.25, delta)
       }
     }
+  })
 
-    // Add and remove the event listener based on isDragEnabled
-    if (isDragEnabled) {
-      document.addEventListener('pointermove', controlShirt);
-    } else {
-      document.removeEventListener('pointermove', controlShirt);
-    }
-  });
-
-  return <group ref={group}>{children}</group>;
-}
-
-
-
-export const DnsToggle = ({toggleDrag}) => {
-
-          <CustomButton
-              type='filled'
-              title={`${!isDragEnabled ? 'Static' : 'Dynamic'}`}
-              handleClick={toggleDrag}
-              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
-            />
-
+  return <group ref={group}>{children}</group>
 }
