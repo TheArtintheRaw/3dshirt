63d8caaa27a3ed8b2b7c255e678cfbb0c066fefc
Roger Andrews <theartintheraw@gmail.com>
2024-01-24 16:59:19 -0500

    Update BuyButton.jsx

--- src/app/components/BuyButton.jsx
+++ src/app/components/BuyButton.jsx
@@ -2,11 +2,13 @@ import Script from 'next/script'
 
 export default function BuyButton() {
   return ( 
+    <>
     <Script id="BuyButton" async src="https://js.stripe.com/v3/buy-button.js"></Script>
     <stripe-buy-button
       buy-button-id="buy_btn_1OcEDZBZCXWBKpoPYkGtwWeT"
       publishable-key="pk_live_51MjrgzBZCXWBKpoPnqjHnrVWc0BDO9tv0ew7PzudLCeJj0W9hDyf1mwr60jolMHap180Y4QyQrdGx5cjX3RNZXkh00WZ3Lr6UZ"
     >
     </stripe-buy-button>
+    </>
   );
 }
